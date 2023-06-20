2dc9d39513ccb5a57832d9581cf1286f3afc5a55
Sakibahmed2 <mauijaloskor@gmail.com>
2023-06-20 09:12:21 +0600

    navbar

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
-import Navbar from '../Pages/Shared/Navbar/Navbar';
 import Footer from '../Pages/Shared/Footer/Footer';
 import NabBar from '../Pages/Shared/Navbar/NabBar';
 
 const Main = () => {
     return (
         <div>
-            <NabBar />
+            <NabBar/>
             <Outlet />
             <Footer />
         </div>
